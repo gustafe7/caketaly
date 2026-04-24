e3c6dbde7df7c6e5e2467aedebc08eae3c1630b6
Gustavo Gomes <gustavocjr.gg@gmail.com>
2026-04-23 22:48:21 -0300

    frontend apontando para o backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Instância do axios apontando para o backend FastAPI
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://caketaly-production.up.railway.app',
 });
 
 export default api;
